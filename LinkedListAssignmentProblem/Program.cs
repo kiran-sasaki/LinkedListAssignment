f35cea5fdf5a223a1d38afc7bcfeae872b00fda3
kiran-sasaki <kiransekar1111@gmail.com>
2022-11-28 15:48:19 +0530

    Added Welcome Messsage For LinkedList Problem

---
+++ LinkedListAssignmentProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkedListAssignmentProblem
+{
+    class Progam
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Problem");
+        }
+    }
+}
